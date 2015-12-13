6bc1c091c6dfa1066646fcdaedaea39f18f7d4eb
Reda Lemeden <git@kaishin.haz.email>
2015-12-14 00:04:41 +0100

    Add CSS rules

---
+++ Nope.safariextension/rules/custom/css_rules.js
@@ -0,0 +1,11 @@
+var advertisingCSSRules = [{
+  "trigger": {
+    "url-filter": ".*"
+  },
+  "action": {
+    "type": "css-display-none",
+    "selector":
+      "[id^='ad-container' i],\
+      [id^='ads-container' i]"
+  }
+}];
